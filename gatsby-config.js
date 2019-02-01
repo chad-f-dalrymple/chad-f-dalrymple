32606649fe5e8b33cb2fdfe2ebcf512dc0e49ae2
Chad <cdalrymple@connectivedx.com>
2019-02-01 17:44:24 -0500

    fixed error in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     author: `Chad Dalrymple`,
   },
   plugins: [
-    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
