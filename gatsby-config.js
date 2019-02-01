ca3ec8da5858c82443e838a6bff79fab8b004d9e
Chad <cdalrymple@connectivedx.com>
2019-02-01 17:29:21 -0500

    for saving purposes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Chad Dalrymple`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
